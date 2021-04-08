6052cfb15d3d238ad8bde26235f8745b46492fca
Justin <justinnguyen0821@gmail.com>
2021-04-07 23:47:21 -0700

    Set up environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+
 def create_app():
+    # Initialize app and load in config/environment settings
     app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    
+    # 
+    db = SQLAlchemy()
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
 
     @app.route('/test')
     def test():
