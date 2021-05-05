ee7a0ac278af7be7641e406616099d3fe7e0ba56
Justin <justinnguyen0821@gmail.com>
2021-05-04 17:12:42 -0700

    Finish setting up config

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,20 @@
-import os
-
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from config import config
 
+# Instatiate flask extensions
+db = SQLAlchemy()
+login_manager = LoginManager()
 
-def create_app():
+def create_app(config_name):
+    ''' Application factory for Flask application '''
     # Initialize app and load in config/environment settings
     app = Flask(__name__)
-    app.config.from_pyfile('config.py')
-    
-    # 
-    db = SQLAlchemy()
-    db.init_app(app)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
 
-    login_manager = LoginManager()
+    db.init_app(app)
     login_manager.init_app(app)
 
     @app.route('/test')
--- app/config.py
+++
@@ -1,4 +0,0 @@
-from os import environ
-
-SECRET_KEY = environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
